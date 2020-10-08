de61f5d698befe44d62daad5dff043bfe51c880a
Sajal Agrawal <59311163+sajal101agrawal@users.noreply.github.com>
2020-10-08 20:56:13 +0530

    Add files via upload

---
+++ Celcius_to_Farenhit_Converter.py
@@ -0,0 +1,5 @@
+# The program to convert the temperature from celcius to farenhit.
+# Created by SAJAL AGRAWAl
+c=float(input("Enter the Temperature in Celcius : "))
+f = c*9/5 + 32
+print (c," Degree Celcius is equal to ",f, " Farenhit ")
